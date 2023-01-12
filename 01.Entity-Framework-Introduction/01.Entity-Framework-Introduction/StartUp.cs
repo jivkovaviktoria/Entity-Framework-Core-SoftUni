e039da863a695bf7d88d25d88749ea1d372828c5
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-12 15:12:31 +0200

    Solved "Adding a new address and updating employee" from Entity framework introduction

--- 01.Entity-Framework-Introduction/01.Entity-Framework-Introduction/StartUp.cs
+++ 01.Entity-Framework-Introduction/01.Entity-Framework-Introduction/StartUp.cs
@@ -3,6 +3,7 @@ using SoftUni.Models;
 using System;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SoftUni
 {
@@ -14,7 +15,9 @@ namespace SoftUni
 
             // Console.WriteLine(GetEmployeesFullInformation(dbContext));
             // Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContext));
-            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContext));
+            // Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContext));
+            // Console.WriteLine(AddNewAddressToEmployee(dbContext));
+            Console.WriteLine(AddNewAddressToEmployee(dbContext));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -72,6 +75,25 @@ namespace SoftUni
             foreach(var em in employees)
                  sb.AppendLine($"{em.FirstName} {em.LastName} from {em.Department.Name} - ${em.Salary:f2}");
             
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address newAddress = new Address() { AddressText = "Vitoshka 15", TownId = 4 };
+            context.Addresses.Add(newAddress);
+
+            var nakov = context.Employees.First(x => x.LastName == "Nakov");
+            nakov.Address = newAddress;
+
+            context.SaveChangesAsync();
+
+            var addressText = context.Employees.OrderByDescending(x => x.AddressId).Take(10)
+                .Select(x => x.Address.AddressText).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var ad in addressText) sb.AppendLine(ad);
+
             return sb.ToString().TrimEnd();
         }
     }
