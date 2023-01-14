a0023bb0b7f74e2277983d4a3e3e43c530baa7b4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-14 23:19:27 +0200

    Solved "Age Restriction" from Advanced Querying

--- _04.AdvancedQuerying/BookShop/StartUp.cs
+++ _04.AdvancedQuerying/BookShop/StartUp.cs
@@ -1,4 +1,9 @@
-﻿namespace BookShop
+﻿using System;
+using System.Linq;
+using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
 {
     using Data;
     using Initializer;
@@ -9,6 +14,28 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            var command = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+        
+        // 2. Age Restriction
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var cmd = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == cmd)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var b in books)
+                sb.AppendLine(b);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
